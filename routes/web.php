48c365687fc505c6c39f34352b129e90c66468c0
AJ-PC\AJ <ali.jibran44@gmail.com>
2017-04-29 18:54:18 +0500

    no message

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,8 @@ $api->version('v1',function($api){
 $api->version('v1',['middleware'=>'tokenCheck'],function($api){
 
     $api->post('getMakes','App\Http\Controllers\ApiController@getMakes');
-    $api->post('getModel','App\Http\Controllers\ApiController@getModel');
-    $api->post('getModelNYear','App\Http\Controllers\ApiController@getModelNYear');
+    $api->get('getModel','App\Http\Controllers\ApiController@getModel');
+    $api->get('getModelNYear','App\Http\Controllers\ApiController@getModelNYear');
 
 
 
