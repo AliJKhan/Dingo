440e47e6919656a59988ad44ad01dc15ef52a3d6
AJ-PC\AJ <ali.jibran44@gmail.com>
2017-05-10 18:12:42 +0500

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,3 @@
 @extends('layouts.headers')
 @extends('layouts.navbar')
 
-
-{{$ownedCar->make_name}}
-{{$ownedCar->model_name}}
-<img src="{{$ownedCar->make_thumbnail}}">
-{{$ownedCar->engine_health}}
-
-
