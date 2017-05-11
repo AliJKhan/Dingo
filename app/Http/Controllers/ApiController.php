926596cc5777ea78ec702e4f844fe68ae409016b
AJ-PC\AJ <ali.jibran44@gmail.com>
2017-05-11 16:46:55 +0500

    no message

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -114,11 +114,16 @@ class ApiController extends Controller
                     $user = User::where('phone_number', $mobile)->first();
 
                     if($user){
-                        return response()->json(['response_code' => ConstantsController::OTP_VERIFIED_USER_ALREADY_EXITS, 'message' => "Pin verified, User already SignedUp", 'data' =>['user_id'=>$user->id,'token'=>$user->token]], 200);
-                    }
-                    $ownedCars = $user->getAllCars;
-                    if(!$ownedCars->first()){
-                        return response()->json(['response_code' => ConstantsController::USER_EXISTS_NO_CARS_FOUND, 'message' => "No Cars Found" , "data"=>""], 200);
+                        $ownedCars = $user->getAllCars;
+                        if(!$ownedCars->first()) {
+                            return response()->json(['response_code' => ConstantsController::USER_EXISTS_NO_CARS_FOUND, 'message' => "No Cars Found" , "data"=>""], 200);
+
+                        }else{
+                            return response()->json(['response_code' => ConstantsController::OTP_VERIFIED_USER_ALREADY_EXITS, 'message' => "Pin verified, User already SignedUp", 'data' => ['user_id' => $user->id, 'token' => $user->token]], 200);
+
+                        }
+
+
                     }
                     $role = Sentinel::findRoleByName('User');
 
