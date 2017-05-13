57ff2630df10874e92ca8c123ebea96c2343cc37
AJ-PC\AJ <ali.jibran44@gmail.com>
2017-05-13 21:46:32 +0500

    no message

---
+++ app/modelnyear_service.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class modelnyear_service extends Model
+{
+    protected $table = 'modelnyear_service';
+    public $timestamps = false;
+}
